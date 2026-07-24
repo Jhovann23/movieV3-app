f9d56172969f04341d77ce73f78b045c78e6f99f
Jhovan <jovanfirmansyah23@gmail.com>
2026-07-25 00:13:36 +0700

    add response dll

--- backend/utils/password.go
+++ backend/utils/password.go
@@ -1,1 +1,15 @@
 package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	return string(bytes), err
+}
+
+func CheckHashPassword(password string, hashPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte(password))
+
+	return err == nil
+}
--- backend/utils/response.go
+++ backend/utils/response.go
@@ -1,1 +1,68 @@
 package utils
+
+import (
+	"github.com/gofiber/fiber/v3"
+)
+
+type Response struct {
+	Status string
+	ResponseCode int
+	Message string
+	Data interface{}
+	Error string
+}
+
+func Success(c fiber.Ctx, message string, data interface{} ) error {
+	return c.Status(fiber.StatusOK).JSON(
+		Response{
+			Status: "Success",
+			ResponseCode: fiber.StatusOK,
+			Message: message,
+			Data: data,
+		},
+	)
+}
+
+func BadRequest(c fiber.Ctx, message string, err error) error {
+	return c.Status(fiber.StatusBadRequest).JSON(
+		Response{
+			Status: "Bad Request",
+			ResponseCode: fiber.StatusBadRequest,
+			Message: message,
+			Error: err.Error(),
+		},
+	)
+}
+
+func NotFound(c fiber.Ctx, message string, err error) error {
+	return c.Status(fiber.StatusNotFound).JSON(
+		Response{
+			Status: "Not Found",
+			ResponseCode: fiber.StatusNotFound,
+			Message: message,
+			Error: err.Error(),
+		},
+	)
+}
+
+func Unauthorized(c fiber.Ctx, message string, err error) error {
+	return c.Status(fiber.StatusUnauthorized).JSON(
+		Response{
+			Status: "Unauthorized",
+			ResponseCode: fiber.StatusUnauthorized,
+			Message: message,
+			Error: err.Error(),
+		},
+	)
+}
+
+func InternalServerError(c fiber.Ctx, message string, err error) error {
+	return c.Status(fiber.StatusInternalServerError).JSON(
+		Response{
+			Status: "Internal Server Error",
+			ResponseCode: fiber.StatusInternalServerError,
+			Message: message,
+			Error: err.Error(),
+		},
+	)
+}
