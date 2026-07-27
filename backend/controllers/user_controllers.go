61204a7971e4cc5ae9120f2330a70c93d455e2d4
Jhovan <jovanfirmansyah23@gmail.com>
2026-07-28 00:01:31 +0700

    progressing

--- backend/controllers/user_controllers.go
+++ backend/controllers/user_controllers.go
@@ -1,1 +1,18 @@
 package controllers
+
+import (
+	"github.com/Jhovann23/movieV3-app/services"
+	"github.com/gofiber/fiber/v3"
+)
+
+type UserController struct {
+	service services.UserService
+}
+
+func NewUserController(s services.UserService) *UserController {
+	return &UserController{s}
+}
+
+func (c *UserController) Register(ctx fiber.Ctx) error {
+
+}
